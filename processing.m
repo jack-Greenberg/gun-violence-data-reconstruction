b31174c372d45737e124e0c10869a2e194a4d0aa
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-02-25 23:39:40 -0500

    Cleaned up data processing file

--- processing.m
+++ processing.m
@@ -1,13 +1,21 @@
-% grab data
-data = readmatrix('clean.csv');
-data(isnan(data)) = 1; % replaces all NaN values with 1, which assumes that there is a minimum of 1 gun involved
+% Import data
+data = readmatrix('./clean.csv');
 
-% perform PCA using SVD
-sv = 30;
+% replaces all NaN values with 1, which assumes that there is a minimum of 1 gun involved
+data(isnan(data)) = 1;
+
+% Perform singular value decomposition on the data
+sv = 80; % Number of singular vectors to use
 [U, sigma, V] = svds(data, sv);
-data_recon = U(:, 1:sv) * sigma(1:sv, 1:sv) * V(:, 1:sv)'
 
-% check for accuracy
-data_recon_stack = reshape(data_recon, [1365 1]);
-data_stack = reshape(data, [1365 1]);
-norm(data_recon_stack - data_stack)
+reconstructed_data = U(:, 1:sv) * sigma(1:sv, 1:sv) * V(:, 1:sv)';
+
+% Reshape reconstructed data into a vector
+reconstructed_data_stack = reshape(reconstructed_data,...
+    [size(reconstructed_data,1)*size(reconstructed_data,2) 1]);
+
+% Reshape original data into a vector
+data_stack = reshape(data, [size(data,1)*size(data,2) 1]);
+
+% Measure distance between original data and reconstructed data
+norm(reconstructed_data_stack - data_stack)
