7c72fd33b54cc8220f1524e5de8fb3284fa3e605
Aydin O'Leary <aydinoleary@gmail.com>
2020-02-29 14:27:08 -0500

    Complete linear regression on test data

--- regression.py
+++ regression.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('./clean_short.csv')
+tr_k = pd.read_csv('training_known.csv').fillna(0)
+tr_u = pd.read_csv('training_unknown.csv').fillna(1)
+te_k = pd.read_csv('test_known.csv').fillna(0)
+te_u = pd.read_csv('test_unknown.csv').fillna(1)
 
+reg = LinearRegression().fit(tr_k, tr_u)
+reconstructed = reg.predict(te_k)
 
+rmse = np.sqrt(((reconstructed - te_u) ** 2).mean())
+
+print(rmse)
